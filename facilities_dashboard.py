6babe246a0ee7dcde758c5b1a76b64d691aedf56
Maryam01-coder <maryamokikiola@gmail.com>
2026-09-10 10:50:38 -0400

    Initial-Commit

---
+++ facilities_dashboard.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+# set up config
+st.set_page_config(
+    page_title="Educational Facilities Dashboard",
+    page_icon="🏫",
+    layout="wide"
+)
